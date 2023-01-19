7b66acc376d4318f75fcec61ea91803afd857547
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2023-01-19 16:24:48 -0300

    uptade header homepage

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -7,29 +7,17 @@ const HomePage = () => {
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1 className="border-l-2 border-white bg-[#5766DA] px-2 py-3 text-2xl font-medium text-white">
+      <h1 className=" bg-[#5766DA] px-2 py-3 text-2xl font-medium text-white">
         Caixa de entrada
       </h1>
-      <div className="h-full bg-white">
-        <div className="grid grid-cols-6">
-          <div className="border-b-2 border-r-2 border-[#5766da7e] py-3  text-center">
-            Nome
-          </div>
-          <div className="border-b-2 border-r-2 border-[#5766da7e] py-3 text-center">
-            E-mail
-          </div>
-          <div className="border-b-2 border-r-2 border-[#5766da7e] py-3 text-center">
-            Atendente
-          </div>
-          <div className="border-b-2 border-r-2 border-[#5766da7e] py-3 text-center">
-            Duração
-          </div>
-          <div className="border-b-2 border-r-2 border-[#5766da7e] py-3 text-center">
-            Status
-          </div>
-          <div className="border-b-2 border-r-2 border-[#5766da7e] py-3 text-center">
-            Data da chamada
-          </div>
+      <div className="flex h-full w-full flex-col bg-white ">
+        <div className="flex space-x-10 bg-[#ABC3FF] py-4 pl-2.5 pr-1.5 text-lg font-semibold text-[#313978]">
+          <div className="min-w-[400px] flex-1">Contatos</div>
+          <div className="flex-1">Duração</div>
+          <div className="flex-1">Agentes</div>
+          <div className="min-w-[250px] flex-1">Departamento</div>
+          <div className="flex-1">Status</div>
+          <div className="flex-1">Criação</div>
         </div>
         <ChatsCell type={'home'} />
       </div>
