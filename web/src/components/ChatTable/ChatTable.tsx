05e96a9fc0ee267dd9e6c12b0853728d718a36ac
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2023-01-24 09:35:00 -0300

    update icon from homepage

--- web/src/components/ChatTable/ChatTable.tsx
+++ web/src/components/ChatTable/ChatTable.tsx
@@ -13,16 +13,15 @@ const formattedDate = (datetime: ConstructorParameters<typeof Date>[0]) => {
 const ChatTable = ({ chat }: Props) => {
   return (
     <Link to={routes.chat({ id: chat.id })}>
-      <div className="flex space-x-10 border-l-2 border-white py-4 pl-2.5 pr-1.5 hover:bg-[#5766DA] ">
-        <div className=" flex">
-          <div className="h-16 w-16">
-            <div className="">
+      <div className="group flex items-center justify-center space-x-10 border-l-2 border-white py-4 pl-2.5 pr-1.5 hover:bg-[#5766DA] ">
+        <div className=" flex h-[60px] min-w-[400px] ">
+          <div className="flex h-16 w-16 items-center justify-center ">
+            <div className="fill-[#5766DA] group-hover:fill-white">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="50"
                 height="50"
-                fill="#5766DA"
-                className="bi bi-person-circle rounded-full "
+                className="bi bi-person-circle rounded-full bg-[#ABC3FF] group-hover:bg-[#5766DA]"
                 viewBox="0 0 16 16"
               >
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
@@ -34,21 +33,27 @@ const ChatTable = ({ chat }: Props) => {
             </div>
           </div>
           <div>
-            <div> {chat.user.name}</div>
-            <div> {chat.user.email}</div>
+            <div className=" text-lg group-hover:text-white">
+              {chat.user.name}
+            </div>
+            <div className="align-top text-base font-light group-hover:text-white">
+              {chat.user.email}
+            </div>
           </div>
         </div>
-        <div className="border-r-2 border-b-2 border-[#5766da7e] py-3 text-center"></div>
-        <div className="border-r-2 border-b-2 border-[#5766da7e] py-3 text-center">
-          {chat.user.name}
+        <div className="h-full flex-1 text-lg group-hover:text-white">
+          {chat.duration} minutos
         </div>
-        <div className="border-r-2 border-b-2 border-[#5766da7e] py-3 text-center">
-          {chat.duration} min
+        <div className="h-full flex-1 group-hover:text-white ">...</div>
+        <div className="h-full min-w-[250px] group-hover:text-white">...</div>
+        <div className=" h-full flex-1">
+          {chat.status === 'active' && (
+            <div className="text-053F04 rounded-full bg-[#A3ECB8] px-5 py-2.5 text-center text-lg font-bold">
+              Aberto
+            </div>
+          )}
         </div>
-        <div className="border-r-2 border-b-2 border-[#5766da7e] py-3 text-center">
-          {chat.status}
-        </div>
-        <div className="border-r-2 border-b-2 border-[#5766da7e] py-3 text-center">
+        <div className="h-full flex-1 text-lg group-hover:text-white">
           {formattedDate(chat.date)}
         </div>
       </div>
@@ -57,6 +62,3 @@ const ChatTable = ({ chat }: Props) => {
 }
 
 export default ChatTable
-
-
-const style = Style
