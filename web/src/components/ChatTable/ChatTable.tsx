75a96da45828ae8658314c933ebb96801beb1fea
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2023-01-24 10:10:55 -0300

    update image icon fro home page

--- web/src/components/ChatTable/ChatTable.tsx
+++ web/src/components/ChatTable/ChatTable.tsx
@@ -16,21 +16,30 @@ const ChatTable = ({ chat }: Props) => {
       <div className="group flex items-center justify-center space-x-10 border-l-2 border-white py-4 pl-2.5 pr-1.5 hover:bg-[#5766DA] ">
         <div className=" flex h-[60px] min-w-[400px] ">
           <div className="flex h-16 w-16 items-center justify-center ">
-            <div className="fill-[#5766DA] group-hover:fill-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="50"
-                height="50"
-                className="bi bi-person-circle rounded-full bg-[#ABC3FF] group-hover:bg-[#5766DA]"
-                viewBox="0 0 16 16"
-              >
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                <path
-                  fillRule="evenodd"
-                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-                />
-              </svg>
-            </div>
+            {chat.user.pfp === '' && (
+              <div className="fill-[#5766DA] group-hover:fill-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  className="bi bi-person-circle rounded-full bg-[#ABC3FF] group-hover:bg-[#5766DA]"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                  />
+                </svg>
+              </div>
+            )}
+            {chat.user.pfp !== '' && (
+              <img
+                src={chat.user.pfp}
+                alt={chat.user.name}
+                className="h-14 w-14 rounded-full object-cover object-center"
+              />
+            )}
           </div>
           <div>
             <div className=" text-lg group-hover:text-white">
@@ -48,8 +57,18 @@ const ChatTable = ({ chat }: Props) => {
         <div className="h-full min-w-[250px] group-hover:text-white">...</div>
         <div className=" h-full flex-1">
           {chat.status === 'active' && (
-            <div className="text-053F04 rounded-full bg-[#A3ECB8] px-5 py-2.5 text-center text-lg font-bold">
+            <div className="rounded-full bg-[#A3ECB8] px-5 py-2.5 text-center text-lg font-bold text-[#053F04]">
               Aberto
+            </div>
+          )}
+          {chat.status === 'close' && (
+            <div className="rounded-full bg-[#D62828] px-5 py-2.5 text-center text-lg font-bold text-[#FFD3D3]">
+              Fechado
+            </div>
+          )}
+          {chat.status === 'waiting' && (
+            <div className="rounded-full bg-[#ABC3FF] px-5 py-2.5 text-center text-lg font-bold text-[#313978]">
+              Pendente
             </div>
           )}
         </div>
--- web/src/components/ChatsCell/ChatsCell.mock.ts
+++ web/src/components/ChatsCell/ChatsCell.mock.ts
@@ -86,7 +86,81 @@ export const standard = (/* vars, { ctx, req } */) => ({
         role: 'Monitor',
       },
       duration: 6,
-      status: 'active',
+      status: 'close',
+      date: '2022-07-14T10:02:37.005Z',
+      messages: [
+        {
+          user_id: 42,
+          message: "Hey, Bob! What's up?",
+          data: '2022-10-14T17:49:37.005Z',
+        },
+        {
+          user_id: 1,
+          message:
+            'Not much, Patrick. Just hanging out at the Krusty Krab, as usual. How about you?',
+          data: '2022-10-14T17:50:37.005Z',
+        },
+        {
+          user_id: 42,
+          message:
+            'Same old, same old. Just lounging on the rock, enjoying the sunshine.',
+          data: '2022-10-14T17:52:37.005Z',
+        },
+        {
+          user_id: 1,
+          message:
+            "Yeah, it's a great day for a little relaxing. Hey, have you heard about that new burger place that just opened up in town?",
+
+          data: '2022-10-14T17:53:37.005Z',
+        },
+        {
+          user_id: 42,
+          message: "No, I haven't. What's it called?",
+          data: '2022-10-14T17:55:37.005Z',
+        },
+        {
+          user_id: 1,
+          message:
+            "It's called the Chum Bucket. I heard they have some pretty good burgers.",
+          data: '2022-10-14T18:10:37.005Z',
+        },
+        {
+          user_id: 42,
+          message:
+            "Really? I'll have to check it out. I'm always up for trying a new burger place.",
+          data: '2022-10-14T18:15:37.005Z',
+        },
+        {
+          user_id: 1,
+          message:
+            'Yeah, me too. Maybe we can go check it out together sometime.',
+          data: '2022-10-14T18:19:37.005Z',
+        },
+
+        {
+          user_id: 42,
+          message: 'Sounds like a plan! See you later, Bob.',
+          data: '2022-10-14T18:20:37.005Z',
+        },
+        {
+          user_id: 1,
+          message: 'See you later, Patrick.',
+          data: '2022-10-14T18:22:37.005Z',
+        },
+      ],
+    },
+    {
+      id: 44,
+      client: 'Fenda do biquini',
+      user: {
+        name: 'Lula Molusco',
+        pfp: '',
+        email: 'gran_molusc@example.com',
+        cell: '0-X5x12A-1515',
+        role: 'Professor',
+      },
+      duration: 35,
+      status: 'waiting',
       date: '2022-07-14T10:02:37.005Z',
       messages: [
         {
